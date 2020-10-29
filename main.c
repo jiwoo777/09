243e26fbe2b3499837ce714324afedf5b211483a
jiwoo01 <gwangbok1004@sookmyung.ac.kr>
2020-10-29 15:32:31 +0900

    practice2-2

--- main.c
+++ main.c
@@ -2,22 +2,23 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
+#define SIZE 5
 
 int main(int argc, char *argv[]) 
 {
 	int i, average;
 	int sum;
-	int grade[5];
+	int grade[SIZE];
 	
 	sum=0;
-	for (i=0; i<5; i++)
+	for (i=0; i<SIZE; i++)
 	{
 		printf("학생 성적을 입력하세요 : ");
 		scanf("%d", &grade[i]);
 		sum+=grade[i];
 	 
 	}
-	average=sum/5;
+	average=sum/SIZE;
 	printf("성적 평균 : %d\n", average);
 	return 0;
 }
